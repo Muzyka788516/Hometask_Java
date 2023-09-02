0928b0dea110b50d007cbb7c71d673d1b18fb967
alinamuzyka <muzyka788516@gmail.com>
2023-09-02 14:26:35 +0200

    add HomeWork1

---
+++ src/main/java/de/telran/lesson2/HomeWork1.java
@@ -0,0 +1,22 @@
+package de.telran.lesson2;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        char a = 'G';
+        int b = 89;
+        byte c = 4;
+        short d = 56;
+        float e = 4.7333436f;
+        double r = 4.355453532;
+        long j = 12121;
+        System.out.println("char: " + a);
+        System.out.println("int:" + b);
+        System.out.println("byte:" + c);
+        System.out.println("short:" + d);
+        System.out.println("float:" + e);
+        System.out.println("double:" +r);
+        System.out.println("long:" +j);
+
+    }
+}
+
---
+++ src/main/java/de/telran/lesson2/HomeWork2.java
@@ -0,0 +1,7 @@
+package de.telran.lesson2;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+
+    }
+}
