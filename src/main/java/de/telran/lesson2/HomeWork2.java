1c97ae9692c2d3b9be82694b52b23ac86714ca3e
alinamuzyka <muzyka788516@gmail.com>
2023-09-02 15:30:12 +0200

    add HomeWork2

--- src/main/java/de/telran/lesson2/HomeWork2.java
+++ src/main/java/de/telran/lesson2/HomeWork2.java
@@ -2,6 +2,13 @@ package de.telran.lesson2;
 
 public class HomeWork2 {
     public static void main(String[] args) {
+        int nummer = 345;
+
+        int hundert = nummer/100;
+        int zeint = nummer%100/10;
+        int eins = nummer%10;
+
+        System.out.println("Число " + nummer + "->" + hundert +"," + zeint +  "," + eins);
 
     }
 }
